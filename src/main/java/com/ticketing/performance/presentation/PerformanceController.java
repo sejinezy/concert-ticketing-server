88e2ee3d0fcfb49fcbe47e6cb2c99ba76ab26c9e
sejin <psj8880@naver.com>
2026-07-13 20:34:20 +0900

    refactor:

--- src/main/java/com/ticketing/performance/presentation/PerformanceController.java
+++ src/main/java/com/ticketing/performance/presentation/PerformanceController.java
@@ -8,7 +8,6 @@ import com.ticketing.performance.presentation.dto.PerformanceUpdateRequest;
 import com.ticketing.support.response.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
